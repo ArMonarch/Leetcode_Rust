8e196624531de1d298321657a74a956ecf11a180
Ar_Monarch <praffulthapa12@gmail.com>
2025-02-09 12:38:19 +0545

    feat: each problem solution moved to its own module

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+// LeetCode Problem 1. [Two Sum](https://leetcode.com/problems/two-sum)
+mod two_sum;
+// LeetCode Problem - 2. [Add Two Numbers](https://leetcode.com/problems/add-two-numbers)
+mod add_two_num;
+// LeetCode Problem - 412. [Fizz Buzz](https://leetcode.com/problems/fizz-buzz)
+mod fizz_buzz;
+// LeetCode Problem - 13. [Roman to Interger](https://leetcode.com/problems/roman-to-integer)
+mod roman_to_number;
+
 fn main() {
-    println!("Hello, world!");
+    println!("LeetCode Problem Solutions!")
 }
